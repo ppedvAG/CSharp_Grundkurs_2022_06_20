c83c5fc0992240fc25eef70f1554c77da4845d06
ppedvLukasK <lukask@ppedv.de>
2022-06-21 11:53:57 +0200

    M007

---
+++ M007/Person.cs
@@ -0,0 +1,21 @@
+﻿namespace M007;
+
+public class Person
+{
+	public int ID { get; set; }
+
+	public Person(int id) => ID = id;
+
+	~Person()
+	{
+		Console.WriteLine($"Person eingesammelt: {ID}");
+	}
+
+	public void PrintID() => Console.WriteLine(ID);
+
+	#region Static
+	public static int Zaehler = 0;
+
+	public static void ZaehlePerson() => Zaehler++;
+	#endregion
+}
---
+++ M007/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace M007
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			for (int i = 0; i < 25; i++)
+			{
+				Person p = new Person(i);
+			}
+
+			GC.Collect(); //Hier GC erzwingen
+			GC.WaitForPendingFinalizers(); //Warte auf alle Destruktoren
+
+			Person person = new Person(2);
+			person.PrintID();
+
+			//Person.PrintID(); nicht möglich, da nicht statisch
+			Person.ZaehlePerson(); //Statische Methode über Typ aufrufen
+
+			//Wertetyp
+			int original = 5;
+			int x = original;
+			original = 10;
+
+			//Referenztyp
+			Person p1 = new Person(1);
+			Person p2 = p1;
+			p1.ID = 10;
+
+			int anzahl = 0; //bei ref muss der Wert initialisiert sein
+			Addiere(4, 5, ref anzahl);
+			Addiere(4, 5, ref anzahl);
+			Addiere(4, 5, ref anzahl);
+		}
+
+		static void Addiere(int z1, int z2, ref int anz) //"Referenz" herstellen, ähnlich wie out
+		{
+			Console.WriteLine(z1 + z2);
+			anz++;
+		}
+	}
+}
