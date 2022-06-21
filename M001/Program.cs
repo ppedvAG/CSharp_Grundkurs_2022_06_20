bbba444457c09d18bd101ff91cee4a7a495d82e8
ppedvLukasK <lukask@ppedv.de>
2022-06-21 15:17:15 +0200

    M001

--- M001/Program.cs
+++ M001/Program.cs
@@ -1,18 +1,7 @@
-﻿using M008;
-
-public class Program
+﻿public class Program
 {
 	static void Main(string[] args)
 	{
 		Console.WriteLine("Hello, World!");
-		AccessModifier am;
-	}
-}
-
-public class Test : AccessModifier
-{
-	public Test()
-	{
-		Console.WriteLine(Lieblingsfarbe); //Protected
 	}
 }
